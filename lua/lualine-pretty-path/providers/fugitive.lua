b39f021aa8a975df7b69358f392ab36b0091c9a7
bwpge <bwpge.dev@gmail.com>
2024-04-07 20:26:01 -0400

    feat: Implement fugitive provider for blame buffers

--- lua/lualine-pretty-path/providers/fugitive.lua
+++ lua/lualine-pretty-path/providers/fugitive.lua
@@ -4,15 +4,18 @@ local utils = require("lualine-pretty-path.utils")
 ---@field super PrettyPath.Provider
 local M = require("lualine-pretty-path.providers.base"):extend()
 
+local function is_hash(s)
+    return s and #s == 40 and s:match("^[%a%d]+$") ~= nil
+end
+
 function M.can_handle(path)
-    return path:match("^fugitive:") ~= nil or vim.bo.filetype == "fugitive"
+    return path:match("^fugitive:") ~= nil or vim.bo.filetype:match("^fugitive") ~= nil
 end
 
 function M:format_path(path)
-    local p = vim.split(path, self.path_sep .. self.path_sep)[3] or ""
+    local p = vim.split(path, self.path_sep .. self.path_sep)[3] or path
     local id = p:match("^(%d+)" .. self.path_sep)
 
-    -- avoid mistaking object hashes for an id
     if id then
         self.id = id
         p = p:gsub("^%d+" .. self.path_sep, "")
@@ -21,19 +24,34 @@ function M:format_path(path)
     return p
 end
 
+function M:split_path()
+    local parts = self.super.split_path(self)
+    if is_hash(parts[1]) then
+        self.hash = table.remove(parts, 1)
+    end
+    return parts
+end
+
 function M:is_diff()
-    return vim.wo.diff or false
+    return vim.wo.diff
+end
+
+function M:is_blame()
+    return vim.bo.filetype == "fugitiveblame"
 end
 
 function M:extract_scheme() end
 
 function M:get_icon()
-    local name = self:is_diff() and "diff" or "git"
-    return { utils.get_icon(name) }
+    local ft = "git"
+    if self:is_diff() then
+        ft = "diff"
+    end
+    return { utils.get_icon(ft) }
 end
 
 function M:render_name()
-    if self.name and #self.name == 40 and self.name:match("^[%a%d]+$") then
+    if is_hash(self.name) then
         return self.hl(self.name, "fugitiveHash")
     end
 
@@ -42,10 +60,17 @@ end
 
 function M:render()
     if self:is_unnamed() then
-        return "Git status"
+        return self.hl("Git status", self.opts.highlights.filename)
+    elseif self:is_blame() then
+        return self.hl("Git blame", self.opts.highlights.filename)
     end
 
     local status = M.super.render(self)
+    if self.hash then
+        status = self.hl(self.hash, "fugitiveHash")
+            .. self.hl(self.opts.path_sep, self.opts.highlights.path_sep)
+            .. status
+    end
     if self.id then
         status = status .. " " .. self.hl(self.id, self.opts.highlights.id)
     end
